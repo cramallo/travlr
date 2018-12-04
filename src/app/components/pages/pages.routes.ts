8ee73755f6718b01c49ce44637f7c85d272d2d92
unknown <cramallo65@gmail.com>
2018-12-04 02:04:25 -0300

    minimos cambios

--- src/app/components/pages/pages.routes.ts
+++ src/app/components/pages/pages.routes.ts
@@ -1,37 +1,43 @@
-import { RouterModule, Routes } from '@angular/router';
-import { PagesComponent } from './pages.component';
-import { GruposComponent} from './grupos/grupos.component';
-import { DetalleGrupoComponent } from './detalle-grupo/detalle-grupo.component';
-import { LoginGuardGuard } from '../../services/guards/login-guard.guard';
-import { FullcalendarComponent } from '../../components/pages/fullcalendar/fullcalendar.component';
-import { EncuestasComponent } from '../../components/pages/encuestas/encuestas.component';
-import { FondoComunComponent } from '../../components/pages/fondo-comun/fondo-comun.component';
-
+import { RouterModule, Routes } from "@angular/router";
+import { PagesComponent } from "./pages.component";
+import { GruposComponent } from "./grupos/grupos.component";
+import { DetalleGrupoComponent } from "./detalle-grupo/detalle-grupo.component";
+import { LoginGuardGuard } from "../../services/guards/login-guard.guard";
+import { FullcalendarComponent } from "../../components/pages/fullcalendar/fullcalendar.component";
+import { EncuestasComponent } from "../../components/pages/encuestas/encuestas.component";
+import { FondoComunComponent } from "../../components/pages/fondo-comun/fondo-comun.component";
 
 const pagesRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        canActivate: [ LoginGuardGuard ],
-        children:[
-            { path: 'grupos', component: GruposComponent },
-            { path: 'grupos/detalle/:id', component:DetalleGrupoComponent,
-                children:[{
-                    path:'calendar',
-                    component:FullcalendarComponent
-                },
-                {
-                    path:'encuestas',
-                    component:EncuestasComponent
-                },
-                {
-                    path:'fondo',
-                    component:FondoComunComponent
-                }
-            ]
-            }
+  {
+    path: "",
+    component: PagesComponent,
+    canActivate: [LoginGuardGuard],
+    children: [
+      { path: "grupos", component: GruposComponent },
+      {
+        path: "grupos/detalle/:id",
+        component: DetalleGrupoComponent,
+        children: [
+          {
+            path: "",
+            component: FullcalendarComponent
+          },
+          {
+            path: "calendar",
+            component: FullcalendarComponent
+          },
+          {
+            path: "encuestas",
+            component: EncuestasComponent
+          },
+          {
+            path: "fondo",
+            component: FondoComunComponent
+          }
         ]
-    }
+      }
+    ]
+  }
 ];
 
-export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
