64fc2c7824f0d5c29196d8f7045a35a6d94415a5
unknown <cramallo65@gmail.com>
2018-12-04 01:56:51 -0300

    submenu listo

--- src/app/components/pages/pages.routes.ts
+++ src/app/components/pages/pages.routes.ts
@@ -3,6 +3,10 @@ import { PagesComponent } from './pages.component';
 import { GruposComponent} from './grupos/grupos.component';
 import { DetalleGrupoComponent } from './detalle-grupo/detalle-grupo.component';
 import { LoginGuardGuard } from '../../services/guards/login-guard.guard';
+import { FullcalendarComponent } from '../../components/pages/fullcalendar/fullcalendar.component';
+import { EncuestasComponent } from '../../components/pages/encuestas/encuestas.component';
+import { FondoComunComponent } from '../../components/pages/fondo-comun/fondo-comun.component';
+
 
 const pagesRoutes: Routes = [
     {
@@ -11,7 +15,21 @@ const pagesRoutes: Routes = [
         canActivate: [ LoginGuardGuard ],
         children:[
             { path: 'grupos', component: GruposComponent },
-            { path: 'grupos/detalle/:id', component:DetalleGrupoComponent}
+            { path: 'grupos/detalle/:id', component:DetalleGrupoComponent,
+                children:[{
+                    path:'calendar',
+                    component:FullcalendarComponent
+                },
+                {
+                    path:'encuestas',
+                    component:EncuestasComponent
+                },
+                {
+                    path:'fondo',
+                    component:FondoComunComponent
+                }
+            ]
+            }
         ]
     }
 ];
